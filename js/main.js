4eea81e6d3548800c4a9787d5b9dc867c0178f26
dawoon-joo <wnekdns5015@gamil.com>
2022-12-10 02:51:06 +0900

    footer=html css 구성 작업완료

--- js/main.js
+++ js/main.js
@@ -104,3 +104,14 @@ function prevSlide() {
     })
 }
 
+//skipNavi
+const skipNavi = document.querySelectorAll('#skipNavi li a');
+
+for(let el of skipNavi){
+    el.addEventListener('focusin',()=>{
+        el.classList.add('on');
+    })
+    el.addEventListener('focusout', ()=>{
+        el.classList.remove('on');
+    })
+}
